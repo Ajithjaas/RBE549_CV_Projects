7e9d934d7c5899bfbdc73fd850e195c823c8364b
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-09-05 11:48:58 -0700

    Inital commit

--- ajayamoorthy_p1/Phase1/Code/Wrapper.py
+++ ajayamoorthy_p1/Phase1/Code/Wrapper.py
@@ -3,12 +3,12 @@
 """
 RBE/CS Fall 2022: Classical and Deep Learning Approaches for
 Geometric Computer Vision
-Project 1: MyAutoPano: Phase 2 Starter Code
+Project 1: MyAutoPano: Phase 1 Starter Code
 
 
 Author(s):
-Lening Li (lli4@wpi.edu)
-Teaching Assistant in Robotics Engineering,
+Shiva Kumar Tekumatla (stekumatla@wpi.edu)
+MS in Robotics Engineering,
 Worcester Polytechnic Institute
 """
 
