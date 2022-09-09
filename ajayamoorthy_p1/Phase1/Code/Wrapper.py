4800186e571a6e85ed2ca3d45c99339bfb822d67
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-09-09 16:42:37 -0700

    ANMS working

--- ajayamoorthy_p1/Phase1/Code/Wrapper.py
+++ ajayamoorthy_p1/Phase1/Code/Wrapper.py
@@ -19,7 +19,7 @@ import numpy as np
 import cv2
 import argparse
 import matplotlib.pyplot as plt 
-from skimage.feature import peak_local_max
+from skimage import feature 
 
 # Add any python libraries here
 
@@ -30,48 +30,39 @@ class MyPano:
 		"""Using Harris Corners to detect the corners in a given image
 		Based on  https://docs.opencv.org/3.4/dc/d0d/tutorial_py_features_harris.html
 		"""
-
 		gray_img = np.float32(cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY))  # converting the image to gray scale and then to float32 
-		
 		dst = cv2.cornerHarris(gray_img,block_size,sobel_size,0.04)
-
 		dst = cv2.dilate(dst,None)
-		# Threshold for an optimal value, it may vary depending on the image.
-		Image[dst>0.01*dst.max()]=[0,0,255]
-
-		# cv2.imshow('dst',Image)
-		# if cv2.waitKey(0) & 0xff == 27:
-		# 	cv2.destroyAllWindows()
-		return Image 
+		dst[dst<0.01*dst.max()] = 0  # Setting below threshold to zero 
+		Image[dst!=0]=[0,0,255]  
+		cv2.imshow('corners',Image)
+		if cv2.waitKey(0) & 0xff == 27:
+			cv2.destroyAllWindows()
+		return Image, dst
 
 	def ANMS(self,C_Img,N_best):
-
 		#C_Image - Corner Score Image 
 		#N_best - Number of best corners needed
-
 		"""The objective of this step is to detect corners such that 
 		they are equally distributed across the image in order to avoid 
 		weird artifacts in warping."""
-
 		#Find all local maxima see: https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_peak_local_max.html
-		coordinates = peak_local_max(C_Img, min_distance=20) #coordinates are stored 
+		coordinates = feature.peak_local_max(C_Img, min_distance=10,threshold_abs=0.1) #coordinates are stored 
 		r = np.zeros((coordinates.shape[0], 1), dtype=np.float32) 
-		best_coordinates = np.zeros((coordinates.shape[0], 2), dtype=np.float32) #stores the best 
-		r[:] = float("inf")
+		r[:] = np.inf #float("inf")
 		index = 0 
+		
 		for i in coordinates:
-
-			best_coordinates[i][0] = i[1]  #inverted row and column indices 
-			best_coordinates[i][1] = i[0]
-			ED = float("inf")
+			yi,xi = i
+			ED = np.inf 
 			for j in coordinates:
-				if (C_Img[j[0], j[1]] > C_Img[i[0], i[1]]):
-					ED = (j[1] - i[1])**2 + (j[0] - i[0])**2
-				if (ED < r[index][2]):
+				yj,xj =j 
+				if (C_Img[yj,xj] > C_Img[yi,xi]):
+					ED = (xj - xi)**2 + (yj - yi)**2
+				if (ED < r[index][0]):
 					r[index][0] = ED
 			index+=1
-		best_coordinates = best_coordinates[np.argsort(r)]
-
+		best_coordinates = coordinates[np.flip(np.argsort(r[:,0]))] #descending order 
 		return best_coordinates[0:N_best,:]
 
 
@@ -86,12 +77,27 @@ def main():
 	image_path = "Phase1/Data/Train/Set1/1.jpg"
 	image_path = "/Users/ric137k/Desktop/Shiva/WPI/Course Work/RBE:CS 549 - Computer Vision /Projects/RBE549_CV_Projects/ajayamoorthy_p1/Phase1/Data/Train/Set1/1.jpg"
 	Image = cv2.imread(image_path)
+	cv2.imshow('input',Image)
+	if cv2.waitKey(0) & 0xff == 27:
+			cv2.destroyAllWindows()
+	# Image = cv2.imread(image_path,0)
 
-	pano = MyPano()
+	panorama = MyPano()
 
-	corner_score_image = pano.corner_detection(Image,2,3)
+	corner_score_image,dst = panorama.corner_detection(Image,2,3)
 
-	print(pano.ANMS(Image,100))
+	best_coordinates = panorama.ANMS(dst,100)
+	Image = cv2.imread(image_path)
+	for coord in best_coordinates:
+		i,j= int(coord[1]),int(coord[0])
+		# Image[i][j] = [0,0,255] 
+		cv2.circle(Image,(i,j),3,255, -1)
+
+	cv2.imshow('anms',Image)
+	if cv2.waitKey(0) & 0xff == 27:
+			cv2.destroyAllWindows()
+
+	# panorama.ANMS(Image,50)
 
     # """
     # Read a set of images for Panorama stitching
