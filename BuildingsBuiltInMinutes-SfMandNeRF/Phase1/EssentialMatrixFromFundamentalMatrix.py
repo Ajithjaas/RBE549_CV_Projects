e771bcc53942cd67e0e91d14fe182d6463f0779c
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-10-23 14:34:32 -0700

    Extract camera pose output

--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/EssentialMatrixFromFundamentalMatrix.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/EssentialMatrixFromFundamentalMatrix.py
@@ -1,8 +1,13 @@
 import numpy as np
+# from scipy.io import  loadmat 
 def EssentialMatrixFromFundamentalMatrix(K, F):
     E = np.transpose(K).dot(F.dot(K))
     u, s, vh = np.linalg.svd(E)
     s = np.diag([1,1,0])
     return u.dot(s.dot(np.transpose(vh)))
 
-    
+if __name__ == "__main__":
+    K = np.loadtxt("/Users/ric137k/Desktop/Shiva/WPI/Course Work/RBE:CS 549 - Computer Vision /RBE549_CV_Projects/BuildingsBuiltInMinutes-SfMandNeRF/P3Data/calibration.txt")
+    F = np.random.randint(10, size = (3,3)) 
+    print(EssentialMatrixFromFundamentalMatrix(K,F))
+
--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/ExtractCameraPose.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/ExtractCameraPose.py
@@ -8,7 +8,13 @@ def ExtractCameraPose(E):
          U.dot(np.transpose(W).dot(VT)),
          U.dot(np.transpose(W).dot(VT))] 
     C=[U[:, 2],-U[:, 2],U[:, 2],-U[:, 2]]
-    
+
     R = [-R[i] if(np.linalg.det(R[i]) < 0) else R[i] for i in range(4)]
     C = [-C[i] if(np.linalg.det(R[i]) < 0) else C[i] for i in range(4)]
     return R, C
+
+if __name__ == "__main__":
+    E = np.random.randint(10, size = (3,3))
+    print(ExtractCameraPose(E))
+
+
