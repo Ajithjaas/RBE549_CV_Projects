692bd5b930536991fe2f5559162657c7429103b0
Reanu-Keeves <55010991+Reanu-Keeves@users.noreply.github.com>
2022-10-29 10:20:48 -0700

    Add linear triangulation
    This is not tested yet

--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/EssentialMatrixFromFundamentalMatrix.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/EssentialMatrixFromFundamentalMatrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 # from scipy.io import  loadmat 
 def EssentialMatrixFromFundamentalMatrix(K, F):
+    # Should the coordinates be normalized ? 
     E = np.transpose(K).dot(F.dot(K))
     u, s, vh = np.linalg.svd(E)
     s = np.diag([1,1,0])
--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/LinearTriangulation.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/LinearTriangulation.py
@@ -6,4 +6,21 @@ def LinearTriangulation(K, C1, R1, C2, R2, x1, x2):
     P1 = K.dot(np.hstack((R1, C1)))
     P2 = K.dot(np.hstack((R2, C2)))
 
+    x1 = np.hstack((x1, np.ones((x1.shape[0], 1)))) #Appending one to x1 and x2 
+    x2 = np.hstack((x2, np.ones((x1.shape[0], 1))))
 
+    # Now we need to find the skew symmetric matrix of these coordinates 
+    s = lambda x: np.array([[0, -x[2], x[1]], [x[2], 0, x[0]], [x[1], x[0], 0]])
+    X = np.zeros((x1.shape[0], 3))
+    for i,x1_i,x2_i in enumerate(zip(x1,x2)):
+        s1 = s(x1_i)
+        s2 = s(x2_i)
+        # Now we need to stack the dot product of this with P1 and P2 and stack them 
+        A = np.vstack((np.dot(s1, P1), np.dot(s2, P2)))
+        # find svd of A 
+        u, s, vh = np.linalg.svd(A)
+        x = vh.T[:, -1]
+        x = np.reshape(x,(len(x), 1))
+        X[i, :] = x
+
+    return X 
