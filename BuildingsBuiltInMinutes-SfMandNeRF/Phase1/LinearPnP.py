04906da4be5cfb03e9d0c86a7c80627ea71adccd
Shiva Kumar Tekumatla <55010991+shivakumar-tekumatla@users.noreply.github.com>
2022-11-01 16:15:54 -0700

    Update LinearPnP
    Previous version has wrong enumeration syntax and wrong index for D array

--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/LinearPnP.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/LinearPnP.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def LinearPnP(X, x, K):
     A =[]
-    for i,Xi,xi in enumerate(zip(X,x)):
+    for i,(Xi,xi) in enumerate(zip(X,x)):
         Xx,Xy,Xz = Xi 
         xx,xy = xi 
         A.append([Xx,Xy,Xz,1,0,0,0,0,-xx*Xx,-xx*Xy,-xx*Xz,-xx])
@@ -16,10 +16,5 @@ def LinearPnP(X, x, K):
     P = np.reshape(p,(3,4))
     U,D,VT = np.linalg.svd(K_inv.dot(P[:,0:3]))
     R = U.dot(VT)
-    T = K_inv.dot(P[:,3:])/D[0,0]
+    T = K_inv.dot(P[:,3:])/D[0]
     return R,T 
-
-
-
-
-
