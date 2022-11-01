1797592c0ebf4ffff68ff4824628e3fe32722e16
Shiva Kumar Tekumatla <55010991+shivakumar-tekumatla@users.noreply.github.com>
2022-11-01 16:33:53 -0700

    Update PnPRansac
    Previously there we syntax errors . This is working now. I changed the threshold to 1. Still do not know if this is returning correct output

--- BuildingsBuiltInMinutes-SfMandNeRF/Phase1/PnPRANSAC.py
+++ BuildingsBuiltInMinutes-SfMandNeRF/Phase1/PnPRANSAC.py
@@ -2,20 +2,21 @@ import numpy as  np
 import random 
 from LinearPnP import LinearPnP
 
-def PnPRANSAC(X,x,K,iterations=100,threshold=0.01):
+def PnPRANSAC(X,x,K,iterations=1000,threshold=1):
     R_best= []
     C_best= []
     n= 0 
     N = X.shape[0] 
     I = np.identity(3)
-    X = np.hstack((X, np.ones((X.shape[0], 1)))).T 
-    for i in iterations:
-        rdm_idx_12 = random.sample(range(N),12) 
-        X12 = X[rdm_idx_12]
+    X = np.hstack((X, np.ones((X.shape[0], 1)))) 
+    XT = X.T
+    for i in range(iterations):
+        rdm_idx_12 = np.transpose(random.sample(range(N),12)) 
+        X12 = X[rdm_idx_12][:,:-1]
         x12 = x[rdm_idx_12]
         R,C = LinearPnP(X12,x12,K)
         P = np.dot(K, np.dot(R, np.hstack((I, -C))))
-        Errors = (P[0].dot(X)/P[2].dot(X)-x[:,0].T)**2 + (P[1].dot(X)/P[2].dot(X)-x[:,1])**2
+        Errors = (P[0].dot(XT)/P[2].dot(XT)-x[:,0].T)**2 + (P[1].dot(XT)/P[2].dot(XT)-x[:,1])**2
         Errors[Errors<=threshold] = 1
         Errors[Errors>threshold] = 0 
         count= np.sum(Errors)
